032333ca497472021f439b6c3a845ccd5a720627
轻剑快马 <xiaoranran1993@outlook.com>
2019-08-02 19:06:34 +0800

    feat: add ga

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,24 @@
 module.exports = {
   title: 'Flutter World',
+  head: [
+    ['meta', { rel: 'keywords', content: 'flutter, resources' }],
+    ['meta', { rel: 'robots', content: 'index,follow' }],
+    ['link', { rel: 'icon', href: '/favicon.png' }]
+  ],
   locales: {},
   themeConfig: {
-    sidebar: 'auto'
+    sidebar: 'auto',
+    lastUpdated: '最后更新'
   },
+  plugins: [
+    '@vuepress/pwa',
+    '@vuepress/active-header-links',
+    [
+      '@vuepress/google-analytics',
+      {
+        ga: 'UA-119658292-3'
+      }
+    ]
+  ],
   evergreen: true
 }
